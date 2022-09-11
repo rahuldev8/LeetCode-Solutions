e24ac79ce2caafacd4ba97e4ac9aacc034b5c333
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-11 23:04:35 +0530

    Shuffle String
    Problem link to try it yourself : https://leetcode.com/problems/shuffle-string/

---
+++ Easy/Shuffle String.java
@@ -0,0 +1,37 @@
+/*
+You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+
+
+Example 1:
+
+
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+Example 2:
+
+Input: s = "abc", indices = [0,1,2]
+Output: "abc"
+Explanation: After shuffling, each character remains in its position.
+ 
+
+Constraints:
+
+s.length == indices.length == n
+1 <= n <= 100
+s consists of only lowercase English letters.
+0 <= indices[i] < n
+All values of indices are unique.*/
+
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        StringBuilder sb = new StringBuilder(s);
+        int l = s.length();
+        for (int i = 0; i < l; i++) {
+            sb.setCharAt(indices[i], s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
