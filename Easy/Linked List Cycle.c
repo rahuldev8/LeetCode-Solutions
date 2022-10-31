9deee1d6e1b91584f6b569d41b39282066ebbc7b
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-31 21:23:13 +0530

    Linked List Cycle
    Problem Link to try it Yourself : https://leetcode.com/problems/linked-list-cycle/

---
+++ Easy/Linked List Cycle.c
@@ -0,0 +1,39 @@
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+ 
+
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+*/
+
+bool hasCycle(struct ListNode *head) {
+    typedef struct ListNode Node;
+    
+    Node* slow = head;//travels one node per iteration
+    Node* fast = head;//travels two nodws per iteration
+    
+    while(fast!=NULL)//if fast(first element) is not null 
+    {
+            fast = fast->next;//go to next element
+        if(fast!=NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        if(fast == slow) return true;
+        
+        //Time Complexity: o(n)
+        //Space Complexity : o(1)
+    }
+    return false;
+    
+}
