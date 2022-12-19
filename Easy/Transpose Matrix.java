b8797c9660762e87bffab531c229d39a34e0bc8a
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-12-19 09:10:48 +0530

    Transpose Matrix
    Problem link to try it Yourself : https://leetcode.com/problems/transpose-matrix/description/

---
+++ Easy/Transpose Matrix.java
@@ -0,0 +1,43 @@
+/*
+Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+
+
+ 
+
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+Example 2:
+
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+*/
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        //Brute Approac using extra matrix
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        //craeting new matrix
+        int[][] res = new int[col][row];
+
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+                //just interchanging their values
+                res[j][i] = matrix[i][j];
+            }
+        }
+
+        return res;
+
+        //Time Complexity : o(n)
+        //Space Complexity : o(n)
+    }
+}
