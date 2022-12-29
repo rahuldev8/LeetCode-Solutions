386ab7fefabf5863133a49a2940c0cdce851f284
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-12-29 23:30:27 +0530

    Update Isomorphic_Strings.java

--- Easy/Isomorphic_Strings.java
+++ Easy/Isomorphic_Strings.java
@@ -21,3 +21,30 @@ class Solution {
         return true;
     }
 }
+
+//Using HashMap
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        //Using HashMap
+
+        Map<Character, Character> map1 = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>(); 
+
+        for(int i=0; i<s.length(); i++)
+        {
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+
+            //mapping current elements
+            if((map1.containsKey(a) && map1.get(a) != b) || (map2.containsKey(b) && map2.get(b) != a))
+                return false;
+
+            map1.put(a, b);
+            map2.put(b, a);    
+        }
+        return true;
+
+        //Time Complexity : O(n)
+        //Space Complexity : O(n)
+    }
+}
