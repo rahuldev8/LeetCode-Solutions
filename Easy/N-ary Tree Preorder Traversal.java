c13391b5f5265ee4eb99988c2eca14add5722f07
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2023-02-17 21:25:10 +0530

    N-ary Tree Preorder Traversal
    Problem link to try it Yourself : https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/

---
+++ Easy/N-ary Tree Preorder Traversal.java
@@ -0,0 +1,43 @@
+/*
+Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+
+ 
+
+Example 1:
+
+
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [1,3,5,6,2,4]
+Example 2:
+
+
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+
+*/
+
+class Solution {
+    public void helper(Node root, List<Integer> ans)
+    {
+        if(root==null)return;
+
+        ans.add(root.val);
+        for(var child : root.children)
+        {
+            helper(child, ans);
+        }
+    }
+    public List<Integer> preorder(Node root) {
+
+        List<Integer> ans = new ArrayList<>();
+        helper(root, ans);
+        return ans;  
+    }
+
+    //Time Complexity : O(n)
+    //Space Complexity : O(n)
+}
