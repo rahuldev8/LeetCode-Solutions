bb5b7f1192c1b3c15849b46bd7918166ffdd63c1
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2023-02-17 21:51:32 +0530

    N-ary Tree Postorder Traversal
    Problem link to try it Yourself : https://leetcode.com/problems/n-ary-tree-postorder-traversal/description/

---
+++ Easy/N-ary Tree Postorder Traversal.java
@@ -0,0 +1,38 @@
+/*
+Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [5,6,3,2,4,1]
+Example 2:
+
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
+
+*/
+
+class Solution {
+    public void helper(Node root, List<Integer> ans)
+    {
+        if(root==null)return;
+
+        for(var child : root.children)
+        {
+            helper(child, ans);
+        }
+        ans.add(root.val);
+    }
+    public List<Integer> postorder(Node root) {
+        List<Integer> ans = new ArrayList<>();
+        helper(root, ans);
+
+        return ans;
+    }
+}
